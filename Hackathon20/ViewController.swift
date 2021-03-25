f1cc65d6733968a3301c7c824e32ca6fa4b35315
IGP <pyltsov@gmail.com>
2021-03-25 17:13:56 +0100

    Initial Commit

---
+++ Hackathon20/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hackathon20
+//
+//  Created by ИГОРЬ on 25/03/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
